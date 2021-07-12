f34e1c9619732caa073fe1ea65f669bb81e06527
rpoins@gmail.com <rpoins@gmail.com>
2021-07-12 17:11:07 +0530

    first commit

---
+++ main/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+from services.emojifier import api
+
+class Index(TemplateView):
+    template_name ='index.html'
+
+    def post(self,request):
+        content = request.POST['content']
+        emoji = api.predict(content)
+
+        context ={
+            'content':content,
+            'emoji':emoji
+        }
+        return render(request,self.template_name,context)
